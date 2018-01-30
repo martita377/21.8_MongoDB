09597c173f523d2b9b9bf7b2065f335d948eab31
Marta P <mapa.startuj@gmail.com>
2018-01-30 23:52:27 +0100

    refactoring application - next step

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+const PORT = process.env.PORT || 5000;
 
-app.get('/', function(req, res){
-    res.render('home');
-});
+const getBooks = (req, res) => {
+ req.db.books
+  .find()
+  .toArray((err, result) => res.render('index', { result }));
+};
 
-app.listen(port);
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-});
+const saveBook = (req, res) => {
+  req.db.books.insert(req.body, function() {
+  res.redirect('/');
+ });
+};
+
+mongoose.connect(
+ 'mongodb://marta:haslo@ds119028.mlab.com:19028/database-1',
+ {},
+ (err, db) => {
+  express()
+   .use(express.static(path.join(__dirname, 'public')))
+   .use(bodyParser.urlencoded({ extended: true }))
+   .use((req, res, next) => {
+    req.db = {
+     books: db.collection('books'),
+    };
+    return next();
+   })
+   .set('views', path.join(__dirname, 'views'))
+   .set('view engine', 'ejs')
+   .get('/', getBooks)
+   .post('/', saveBook)
+   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+ }
+);
