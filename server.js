06abf64cefb83c6968ad8673ae0d142e12a750b9
Marta P <mapa.startuj@gmail.com>
2018-01-30 19:30:32 +0100

    refactoring application - step 2

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
